d469d4365bd075e39fef703ab6e7cd56796dd75b
David Morton <55235981+davidmorton0@users.noreply.github.com>
2021-06-14 23:36:24 +0100

    add supply center to province

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_222303) do
+ActiveRecord::Schema.define(version: 2021_06_14_223436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_222303) do
     t.string "abbreviation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "supply_center"
   end
 
 end
