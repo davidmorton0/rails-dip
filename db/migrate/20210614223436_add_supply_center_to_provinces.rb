f9f9ad6974642a4a29a619390707301b93e05852
David Morton <davidmorton0@gmail.com>
2021-06-14 23:36:24 +0100

    add supply center to province

---
+++ db/migrate/20210614223436_add_supply_center_to_provinces.rb
@@ -0,0 +1,5 @@
+class AddSupplyCenterToProvinces < ActiveRecord::Migration[6.1]
+  def change
+    add_column :provinces, :supply_center, :boolean
+  end
+end
--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Province, type: :model do
   subject { described_class.new(params) }
 
-  let(:params){ { name: 'Munich', abbreviation: 'MUN' } }
+  let(:params){ { name: 'Munich', abbreviation: 'MUN', supply_center: true } }
 
   it 'returns the province name' do
     expect(subject.name).to eq 'Munich'
@@ -11,5 +11,9 @@ RSpec.describe Province, type: :model do
 
   it 'returns the province abbreviation' do
     expect(subject.abbreviation).to eq 'MUN'
+  end
+
+  it 'returns the province abbreviation' do
+    expect(subject.supply_center?).to eq true
   end
 end
