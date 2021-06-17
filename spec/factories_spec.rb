3104cfd3dbc7ee97dd8b4c227b8d2c05093283b0
David Morton <davidmorton0@gmail.com>
2021-06-17 19:53:46 +0100

    add factories

---
+++ spec/factories_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe FactoryBot do
+  it { FactoryBot.lint(traits: true) }
+end
+
+FactoryBot.factories.map(&:name).each do |factory_name|
+  describe "The #{factory_name} factory" do
+     it 'is valid' do
+      expect(build(factory_name)).to be_valid
+     end
+  end
+end
