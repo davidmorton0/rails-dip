4476cb80b5ab7922f5cd2d489fae23f6c8e96d3b
David Morton <55235981+davidmorton0@users.noreply.github.com>
2021-06-18 23:24:47 +0100

    add check orders unit

---
+++ db/migrate/20210618171420_add_year_season_and_game_to_orders.rb
@@ -0,0 +1,7 @@
+class AddYearSeasonAndGameToOrders < ActiveRecord::Migration[6.1]
+  def change
+    add_column :orders, :year, :integer
+    add_column :orders, :season, :string
+    add_column :orders, :game, :integer
+  end
+end
