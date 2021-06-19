62b428b5d13726cf27ed20c7ea0eb3a3e1103572
David Morton <davidmorton0@gmail.com>
2021-06-19 23:07:52 +0100

    Army cannot move to water

--- app/services/check_orders.rb
+++ app/services/check_orders.rb
@@ -5,7 +5,8 @@ class CheckOrders
 
   def call
     orders.each do |order|
-      result = provinces_adjacent?(order.unit.province, order.target_province)
+      result = provinces_adjacent?(order.unit.province, order.target_province) &&
+        correct_province_type?(order.unit.unit_type, order.target_province.province_type)
       order.update(success: result, fail_reason: @fail_reasons)
     end
   end
@@ -21,5 +22,12 @@ class CheckOrders
       @fail_reasons = 'Target province not adjacent'
       false
     end
+  end
+
+  def correct_province_type?(unit_type, province_type)
+    return true if unit_type == 'Army' && ['Inland', 'Coastal'].include?(province_type)
+    
+    @fail_reasons = 'Army cannot move to Water province'
+    false
   end
 end
