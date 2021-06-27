ba41b301094136b16b6d8d8760310a9c19499e8e
David Morton <davidmorton0@gmail.com>
2021-06-27 08:26:38 +0100

    add process turn to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,15 @@ class Game < ApplicationRecord
 
   validates :map, :year, presence: true
   validates :season, inclusion: { in: %w[Spring Autumn Winter] }
+
+  def process_turn
+    self.season = next_season(season)
+    self.year += 1 if season == 'Spring'
+  end
+
+  def next_season(current_season)
+    seasons = %w[Spring Autumn Winter]
+    current = seasons.index(current_season)
+    seasons[(current + 1) % 3]
+  end
 end
