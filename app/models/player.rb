d60c2458837270d498e8e9e72fb9c4b92ef7f607
David Morton <davidmorton0@gmail.com>
2021-06-30 23:26:59 +0100

    can assign build order

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,10 +5,23 @@ class Player < ApplicationRecord
 
   validates :country, :game, presence: true
 
-  def assign_move_order(order_details)
+  def assign_move_order(current_province:, target_province:, year:, season:)
     MoveOrder.create(
       player: self,
-      **order_details,
+      current_province: current_province,
+      target_province: target_province,
+      year: year,
+      season: season,
+    )
+  end
+
+  def assign_build_order(province:, unit_type:, year:, season:)
+    BuildOrder.create(
+      player: self,
+      province: province,
+      year: year,
+      season: season,
+      unit_type: unit_type,
     )
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,13 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  let(:order_details) do
-    { player: player,
-      season: 'Autumn',
-      year: '1901',
-      target_province: target_province,
-      current_province: current_province }
-  end
+  subject { described_class.new(country: 'Turkey', game: build(:game)) }
+
   let(:current_province) { create(:province) }
   let(:target_province) { create(:province) }
   let(:player) { create(:player) }
@@ -17,14 +12,43 @@ RSpec.describe Player, type: :model do
   it { is_expected.to validate_presence_of(:game) }
   it { is_expected.to validate_presence_of(:country) }
 
-  it 'assigns a move order' do
-    expect { subject.assign_move_order(order_details) }.to change(MoveOrder, :count).by(1)
+  context 'when a move order is assigned' do
+    let(:order_details) do
+      { season: 'Autumn',
+        year: '1901',
+        target_province: target_province,
+        current_province: current_province }
+    end
+    it 'assigns a move order' do
+      expect { subject.assign_move_order(**order_details) }.to change(MoveOrder, :count).by(1)
 
-    expect(MoveOrder.last).to have_attributes(
-      player: player,
-      target_province:
-      target_province,
-      current_province: current_province,
-    )
+      expect(MoveOrder.last).to have_attributes(
+        player: subject,
+        target_province: target_province,
+        current_province: current_province,
+        season: 'Autumn',
+        year: 1901,
+      )
+    end
+  end
+
+  context 'when a build order is assigned' do
+    let(:order_details) do
+      { unit_type: 'Army',
+        season: 'Autumn',
+        year: '1901',
+        province: current_province }
+    end
+    it 'assigns a build order' do
+      expect { subject.assign_build_order(**order_details) }.to change(BuildOrder, :count).by(1)
+
+      expect(BuildOrder.last).to have_attributes(
+        player: subject,
+        unit_type: 'Army',
+        season: 'Autumn',
+        year: 1901,
+        province: current_province,
+      )
+    end
   end
 end
