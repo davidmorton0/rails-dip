9b5983ea872db879142f744838a9c4fa0690d2f3
David Morton <davidmorton0@gmail.com>
2021-07-04 08:49:16 +0100

    can setup new game

--- spec/services/setup_new_game_spec.rb
+++ spec/services/setup_new_game_spec.rb
@@ -3,13 +3,32 @@ require 'rails_helper'
 RSpec.describe SetupNewGame do
   subject { described_class.new(variant: variant) }
 
-  let(:variant) { build(:variant) }
-
+  let(:variant) do
+    build(:variant, starting_year: starting_year, starting_season: starting_season, countries: countries)
+  end
+  let(:starting_year) { 1800 }
+  let(:starting_season) { 'Winter' }
+  let(:countries) { %w[England Russia Italy] }
+  
   before {  }
 
   it 'creates a new game' do
     expect { subject.call }.to change(Game, :count).by(1)
     game = Game.last
-    expect(game.players.count).to eq 7
+    
+    expect(game).to have_attributes(
+      variant: variant,
+      year: starting_year,
+      season: starting_season
+    )
+  end
+
+  it 'creates players' do
+    expect { subject.call }.to change(Player, :count).by(3)
+    players = Player.all
+
+    expect(players).to include(an_object_having_attributes(country: 'England'),
+                               an_object_having_attributes(country: 'Russia'),
+                               an_object_having_attributes(country: 'Italy'))
   end
 end
