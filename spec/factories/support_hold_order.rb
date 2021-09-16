898c417bff7298bde6c63bd6ef25c17317d18b09
David Morton <55235981+davidmorton0@users.noreply.github.com>
2021-09-16 22:05:17 +0100

    add support orders

---
+++ spec/factories/support_hold_order.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :support_hold_order do
+    year { 1901 }
+    season { 'Spring' }
+    player { build(:player) }
+    origin_province { build(:province) }
+    target_province { build(:province) }
+  end
+end
